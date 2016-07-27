7262c9d1c797a85407ee7278cba7fe1de9411ef9
Jackie He <Jackie.He8@gmail.com>
2016-07-27 15:22:28 +0000

    Cleared edge cases.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 $(document).ready(function() {
     $("button").click(function() {
         var input = $("#pass").val()
-        if ((input == "ScriptEd") || (input == "scripted") || (input == "Scripted") || (input == "scriptEd")) {
+        // if ((input == "ScriptEd") || (input == "scripted") || (input == "Scripted") || (input == "scriptEd") || (input == "SC")) {
+        if (input.toLowerCase() == "scripted") {
             $("#result").text("WOOOOOOOHHHH!!!!! You guessed the secret word!");
         }
         else {
